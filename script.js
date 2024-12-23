34aae9e26f42c47241f2501c97abcb84d8f48890
oladosuabayomi <oladosuabayomi6@gmail.com>
2024-12-23 16:48:02 +0100

    Challenge #7

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ checkWinner(averageScoreKoalas, averageScoreDolphins);
 //     console.log(`${johnSmith.fullName}'s BMI ${johnSmith.BMI} is higher than ${markMiller.fullName}'s ${markMiller.BMI}`);
 // }
 
-
+/*
 // Challange #7 (Improving the tip calculator)
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
@@ -212,3 +212,41 @@ const calcAverage = function (arr) {
 }
 
 console.log(calcAverage(totals));
+
+ */
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+/* const arr = [17, 21, 23]; */
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+/* console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ... ${data1[3]}ºC`); */
+
+const printForecast = function (arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}ºC in ${[i + 1]} days ... `;
+    }
+
+    console.log('...' + str);
+};
+printForecast(data1);
+
+
+
+
